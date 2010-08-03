586743dc55f87fba843c437d63aae5198c4e6692
Xander Groeneveld <xander.groeneveld@mail.hsa.net>
2010-08-03 11:08:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoCap::Application.config.secret_token = 'ee438474888238285339cb03e6411e9e21e02571325432c882b36f0cdafb2d35aa615314eb5c1f18fce0c6eaabe8c5430fd7e4884b22c1ad03d533cc769eaa85'

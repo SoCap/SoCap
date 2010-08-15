ef793b4502fa3b6e51029c581e50e47e4b9ec084
Xander Groeneveld <xander.groeneveld@mail.hsa.net>
2010-08-15 14:59:53 -0400

    setting up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ SoCap::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
+  config.action_mailer.default_url_options = { :host => 'www.clikwit.com' }
+  
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 

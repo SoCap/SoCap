be68683d37d23dd28ffe2c332b67ad1d90c66f45
Xander Groeneveld <xander.groeneveld@mail.hsa.net>
2010-08-15 16:05:23 -0400

    changed email again

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "clikwit@gmail.com"
+  config.mailer_sender = "welcome@clickwit.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
